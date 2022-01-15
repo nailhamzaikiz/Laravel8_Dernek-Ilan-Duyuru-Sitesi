50fedd0c0b3387c711441ebd3f26f237bcf4ead0
nailhamzaikiz <nailhamzaikiz@hotmail.com>
2022-01-15 23:15:57 +0300

    commit

--- resources/views/livewire/search.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-    <input wire:model="search" name="search" type="text" class="input-group" list="mylist" placeholder="Arama Yapın." />
-    @if(!empty($query))
-</div>
